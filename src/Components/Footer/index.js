d65eb0840060520ccaf11fe5923a6de1d433b5a5
binavgautam <binav.gautam.512@gmail.com>
2021-03-20 12:22:54 +0530

    First

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Footer from "./footer";
+
+export default function index() {
+  return (
+    <Footer>
+      <Footer.Title>Questions? Contact Us</Footer.Title>
+      <Footer.Break />
+      <Footer.Row>
+        <Footer.Column>
+          <Footer.Link href="#">FAQ</Footer.Link>
+          <Footer.Link href="#">Investors</Footer.Link>
+          <Footer.Link href="#">Ways to Watch</Footer.Link>
+          <Footer.Link href="#">Corporate Info</Footer.Link>
+          <Footer.Link href="#">Terms of Use</Footer.Link>
+        </Footer.Column>
+
+        <Footer.Column>
+          <Footer.Link href="#">Help</Footer.Link>
+          <Footer.Link href="#">Unable to Sign in</Footer.Link>
+          <Footer.Link href="#">Show not loading</Footer.Link>
+          <Footer.Link href="#">Contact Us</Footer.Link>
+        </Footer.Column>
+
+        <Footer.Column>
+          <Footer.Link href="#">Account</Footer.Link>
+          <Footer.Link href="#">See all Plans</Footer.Link>
+          <Footer.Link href="#">Gift Cards</Footer.Link>
+          <Footer.Link href="#">Prefrences</Footer.Link>
+        </Footer.Column>
+      </Footer.Row>
+      <Footer.Break />
+      <Footer.Title>Netlix USA</Footer.Title>
+    </Footer>
+  );
+}
